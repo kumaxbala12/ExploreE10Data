9e57e5fd4bb6cbea520731d9417c2f5f56c273fa
Kuma Bala <kumaxbala@gmail.com>
2025-07-16 13:12:11 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,42 +1,62 @@
 import streamlit as st
 import scanpy as sc
+import anndata as ad
+import pandas as pd
+import matplotlib.pyplot as plt
+from io import BytesIO
 import gdown
 import os
-import anndata as ad
-import matplotlib.pyplot as plt
-from anndata import read as read_h5ad
 
+# ---- CONFIG ----
+st.set_page_config(page_title="scRNA-seq Viewer", layout="wide")
+st.title("scRNA-seq Data Explorer")
 
-st.set_page_config(layout="wide")
-st.title("E10 scRNA-seq Data Explorer")
+# ---- DOWNLOAD FROM GOOGLE DRIVE ----
+DATA_URL = "https://drive.google.com/uc?id=1drzl3mGt_3nKnIDRqs8cRthV6HnoXuwY"
+LOCAL_H5AD = "data.h5ad"
 
-DATA_PATH = "data/e10adata.h5ad"
-GDRIVE_URL = "https://drive.google.com/uc?id=1drzl3mGt_3nKnIDRqs8cRthV6HnoXuwY"
+if not os.path.exists(LOCAL_H5AD):
+    with st.spinner("Downloading data..."):
+        gdown.download(DATA_URL, LOCAL_H5AD, quiet=False)
 
-@st.cache_data
+# ---- LOAD DATA ----
+@st.cache_resource
 def load_data():
-    if not os.path.exists(DATA_PATH):
-        os.makedirs("data", exist_ok=True)
-        gdown.download(GDRIVE_URL, DATA_PATH, quiet=False)
-    return sc.read_h5ad(DATA_PATH)
+    return sc.read_h5ad(LOCAL_H5AD)
 
 adata = load_data()
 
-clusters = sorted(adata.obs['leiden'].unique()) if 'leiden' in adata.obs else []
-selected_clusters = st.multiselect("Select clusters", clusters, default=clusters)
+# ---- FILTERING ----
+cluster_key = "seurat_clusters"
+adata.obs[cluster_key] = adata.obs[cluster_key].astype(str)
+all_clusters = sorted(adata.obs[cluster_key].unique())
+selected_clusters = st.multiselect("Select clusters to include", all_clusters, default=all_clusters)
 
-filtered_data = adata[adata.obs['leiden'].isin(selected_clusters), :] if 'leiden' in adata.obs else adata
+filtered = adata[adata.obs[cluster_key].isin(selected_clusters)]
 
-st.write(f"Showing {filtered_data.n_obs} cells and {filtered_data.n_vars} genes.")
+# ---- PLOTS ----
+st.subheader("Plots")
 
-gene = st.text_input("Enter gene name to visualize (e.g., Sox2)")
-if gene and gene in filtered_data.var_names:
-    sc.pl.violin(filtered_data, keys=gene, groupby='leiden', show=False)
-    st.pyplot(plt.gcf())
-    plt.clf()
+# Gene input
+gene = st.text_input("Enter gene name (case-sensitive):")
 
-    sc.pl.dotplot(filtered_data, var_names=[gene], groupby="leiden", show=False)
-    st.pyplot(plt.gcf())
-    plt.clf()
-elif gene:
-    st.warning(f"{gene} not found in dataset.")
+# Layout: columns
+col1, col2 = st.columns(2)
+
+with col1:
+    st.markdown("### Violin Plot")
+    if gene:
+        fig, ax = plt.subplots()
+        sc.pl.violin(filtered, keys=gene, groupby=cluster_key, ax=ax, show=False)
+        st.pyplot(fig)
+
+with col2:
+    st.markdown("### Dot Plot")
+    if gene:
+        fig = sc.pl.dotplot(filtered, var_names=[gene], groupby=cluster_key, show=False)
+        st.pyplot(fig.figure)
+
+# ---- Metadata Preview ----
+st.subheader("Metadata")
+st.dataframe(adata.obs.head())
+
